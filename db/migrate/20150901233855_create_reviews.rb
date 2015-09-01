5dff54a24b7cc8668aa9cc84229b735a60869af0
Cesar <cesar.ferrat1@gmail.com>
2015-09-01 19:50:15 -0400

    added routes controllers models association

---
+++ db/migrate/20150901233855_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.text :description
+      t.date :date
+      t.integer :rating
+
+      t.timestamps null: false
+    end
+  end
+end
